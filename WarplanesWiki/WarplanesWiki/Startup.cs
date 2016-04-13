88746d12b72c699d8c7ad659d87f241d50946e33
qpPyKT79 <el_razor@mail.ru>
2016-04-13 22:14:18 +0500

    init

---
+++ WarplanesWiki/WarplanesWiki/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WarplanesWiki.Startup))]
+namespace WarplanesWiki
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
