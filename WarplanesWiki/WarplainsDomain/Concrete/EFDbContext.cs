d2bfbc065bcbe7f3fec41483f8bcf8c80115140d
qpPyKT79 <el_razor@mail.ru>
2016-04-19 20:30:47 +0500

    db conn added

---
+++ WarplanesWiki/WarplainsDomain/Concrete/EFDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarplainsDomain.Entities;
+
+namespace WarplainsDomain.Concrete
+{
+    public class EFDbContext : DbContext
+    {
+        public DbSet<Warplane> Warplanes { get; set; }
+
+    }
+}
---
+++ WarplanesWiki/WarplainsDomain/Concrete/EFWarplaneRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarplainsDomain.Abstract;
+using WarplainsDomain.Entities;
+
+namespace WarplainsDomain.Concrete
+{
+    public class EFWarplaneRepository : IWarplaneRepository
+    {
+        private EFDbContext context = new EFDbContext();
+        public IQueryable<Warplane> Warplanes
+        {
+            get { return context.Warplanes; }
+        }
+    }
+}
